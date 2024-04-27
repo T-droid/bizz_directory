3f9d38641f21d571bda90e337929ed486a3c87b6
emmanuel tindi <emmanueltindi66@gmail.com>
2024-04-27 22:36:02 +0300

    files update

--- frontend/biz_app/src/app/login/login.component.ts
+++ frontend/biz_app/src/app/login/login.component.ts
@@ -2,11 +2,17 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
   styleUrl: './login.component.css'
 })
 export class LoginComponent {
+  username: string = '';
+  password: string = '';
 
+  constructor() {}
+  onLogin(){
+    console.log(this.username);
+    console.log(this.password);
+  }
+  
 }
--- frontend/biz_app/src/app/register/register.component.ts
+++ frontend/biz_app/src/app/register/register.component.ts
@@ -2,11 +2,24 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-register',
-  standalone: true,
-  imports: [],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
 export class RegisterComponent {
+  username: string = '';
+  email: string = '';
+  password: string = '';
+  confirmpassword: string = '';
 
-}
+  register(): void {
+    if (this.password !== this.confirmpassword) {
+      // Passwords do not match, handle error (e.g., display error message)
+      console.error("Passwords do not match");
+      return;
+      }
+      console.log('Username:', this.username);
+      console.log('Email:', this.email);
+      console.log('Password:', this.password);
+      console.log('Confirm Password:', this.confirmpassword);
+    }
+  }
